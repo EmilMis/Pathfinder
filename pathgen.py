f03f08ecd8817c566d48cc5cec2c0541a54ad43e
EmilMis <77896233+EmilMis@users.noreply.github.com>
2022-03-25 11:42:47 +0100

    Update pathgen.py

--- pathgen.py
+++ pathgen.py
@@ -2,14 +2,14 @@ import random
 
 #config------------------
 
-x = 20
-y = 20
-mut = 4
+x, y, mut = input("> ").split(' ')
+x = int(x)
+y = int(y)
+mut = int(mut)
 
 #config------------------
 
 s = []
-
 for i in range(x):
     ss = []
     for j in range(y):
@@ -19,6 +19,8 @@ for i in range(x):
 for i in range(x):
     for j in range(mut):
         s[i][random.randint(0, y - 1)] = '#'
+
+s[0][0] = '.'
 
 for l in s:
     sss = ""
